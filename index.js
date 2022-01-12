3f4171072f953d4de2bd13f52ab11623571f1c0a
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2022-01-12 09:41:28 -0600

    Install dotenv & express

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Hola Mundo');
+const express = require('express');
+require( 'dotenv' ).config();
+
+// Crear el servidor
+const app = express();
+
+// Directori Publico
+
+app.use( express.static('public') );
+
+// Rutas
+
+  //  codigo app.get( '/', ( req, res ) => {
+  //  codigo    console.log('Se require el /');
+  //  codigo     res.json({
+  //          ok: true
+  //      });
+  //  });
+
+
+// Escuchar las peticiones
+app.listen( process.env.PORT,  () => {
+
+    console.log( `Servidor corriendo en puerto: ${ process.env.PORT }` );
+
+});
