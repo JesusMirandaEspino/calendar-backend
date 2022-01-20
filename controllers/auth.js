e488a37a7d9cd930bd4446f64a57a619da5e0948
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2022-01-20 08:50:55 -0600

    Update

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,6 +30,16 @@ const loginUsuario = ( req, res ) => {
 
     const { email, password } = req.body;
 
+    const errors = validationResult( req );
+
+        if( !errors.isEmpty() ){
+            return res.status(400).json({
+                ok: false,
+                errors: errors.mapped
+            });
+        }
+
+
         res.json({
             ok: true,
             msg: 'login',
