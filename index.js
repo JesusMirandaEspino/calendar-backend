c16f6dada74924b1664bfd67da79f78833043219
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2022-02-02 09:19:27 -0600

    use cors

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { dbConnection } = require('./database/config');
 require( 'dotenv' ).config();
 
+const cors = require('cors')
+
 // Crear el servidor
 const app = express();
 
@@ -11,6 +13,9 @@ app.use( express.static('public') );
 
 // Conexion a base de datos
 dbConnection();
+
+// cors
+app.use( cors() );
 
 // Lectura y parse del Body
 app.use( express.json() );
